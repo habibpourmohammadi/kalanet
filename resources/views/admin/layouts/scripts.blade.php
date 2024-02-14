62c912725cd65bfd2040cad7bf65f02c14bd05e4
Habib <habibpourmohammady@gmail.com>
2024-02-14 16:53:31 +0330

    admin panel template, added

---
+++ resources/views/admin/layouts/scripts.blade.php
@@ -0,0 +1,6 @@
+<script src="{{ asset('admin-assets/js/jquery-3.5.1.min.js') }}"></script>
+<script src="{{ asset('admin-assets/js/popper2.js') }}"></script>
+<script src="{{ asset('admin-assets/js/bootstrap/bootstrap.min.js') }}"></script>
+<script src="{{ asset('admin-assets/js/grid.js') }}"></script>
+<script src="{{ asset('admin-assets/select2/js/select2.min.js') }}"></script>
+<script src="{{ asset('admin-assets/sweetalert/js/sweetalert2.all.min.js') }}"></script>
---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,30 @@
+<aside id="sidebar" class="sidebar">
+    <section class="sidebar-container">
+        <section class="sidebar-wrapper">
+
+            <a href="{{ route('admin.index') }}" class="sidebar-link">
+                <i class="fas fa-home"></i>
+                <span>خانه</span>
+            </a>
+
+            <section class="sidebar-part-title">تست</section>
+
+            <section class="sidebar-group-link">
+                <section class="sidebar-dropdown-toggle">
+                    <i class="fas fa-chart-bar icon"></i>
+                    <span>تست</span>
+                    <i class="fas fa-angle-left angle"></i>
+                </section>
+                <section class="sidebar-dropdown">
+                    <a href="">تست</a>
+                </section>
+            </section>
+
+            <section class="sidebar-part-title">تست</section>
+            <a href="" class="sidebar-link">
+                <i class="fas fa-bars"></i>
+                <span>تست</span>
+            </a>
+        </section>
+    </section>
+</aside>
